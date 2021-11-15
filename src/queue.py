91e5b2f30de727602d3ba89416dc88d7ce3eba8e
atagiev <andrej-tagiev@yandex.ru>
2021-11-15 17:58:40 +0300

    Create queue

--- src/queue.py
+++ src/queue.py
@@ -7,13 +7,14 @@ class Queue:
         self.__list_closed_requests: list[Request] = []
 
     def add_open_request(self, request: Request):
-        pass
+        self.__list_open_requests.append(request)
 
     def get_open_request(self) -> Request:
-        pass
+        return self.__list_open_requests.pop() if len(self.__list_open_requests) > 0 else None
 
     def move_open_to_closed(self, request: Request):
-        pass
+        self.__list_closed_requests.append(request)
+        self.__list_open_requests.remove(request)
 
     def get_closed_request(self) -> Request:
-        pass
+        return self.__list_closed_requests.pop() if len(self.__list_closed_requests) > 0 else None
