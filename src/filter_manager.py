e05968cf6c3666d918adc3df9a51f96f3a9a01f0
atagiev <andrej-tagiev@yandex.ru>
2021-11-17 00:08:31 +0300

    Added saving logs to file Added log info Fixed FilterManager thread starting

--- src/filter_manager.py
+++ src/filter_manager.py
@@ -18,31 +18,41 @@ class FilterManager:
 
     def import_filters(self):
         # todo import Neural networks and smth like this
+        info(msg="Checking images with styles")
         for filter_image in FilterNamesEnum:
             if filter_image.name.startswith("ai_"):
                 if self.__storage.get_filter_image(filter_name=filter_image) is None:
                     error(f"Filter {filter_image.value} not found !")
                 else:
                     info(msg=f"Filter {filter_image.value} found")
+        info(msg="Checking images with styles completed")
 
     def run(self):
         def run():
             while True:
                 request = self.__queue.get_open_request()
                 if request is None:
-                    sleep(secs=2)
+                    sleep(2)
                     continue
+                info(msg=f"Processing a request {request.input_image_id}")
                 image = self.__storage.get_image(image_id=request.input_image_id)
                 if request.is_ai_filter:
+                    info(msg=f"Applying AI Filter {request.filter_name} to request {request.input_image_id}")
                     ai_filter_image = self.__storage.get_filter_image(filter_name=request.filter_name)
                     result = self.__ai_filter.apply_filter(image=image, filter_name=request.filter_name,
                                                            filter_image=ai_filter_image)
                 else:
+                    info(msg=f"Applying Classic Filter {request.filter_name} to request {request.input_image_id}")
                     result = self.__classic_filter.apply_filter(image=image, filter_name=request.filter_name)
                 result_id = self.__storage.save_image(result)
 
                 request.output_image_id = result_id
+
+                info(msg=f"Output image for request {request.input_image_id} has id {request.output_image_id}")
+                info(msg=f"Deleting image with id {request.input_image_id}")
                 self.__storage.delete_image(image_id=request.input_image_id)
+                info(msg=f"Moving request {request.input_image_id} to closed")
                 self.__queue.move_open_to_closed(request=request)
 
-        threading.Thread(target=run)
+        info(msg="Starting FilterManager thread")
+        threading.Thread(target=run).start()
