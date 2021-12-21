924f371b4b0d49d2838605ba3a5433cd382df059
Mark-Sherman-SE <marshann2012@gmail.com>
2021-12-21 22:30:25 +0300

    Fixed processing of filter names

--- src/ai_filter.py
+++ src/ai_filter.py
@@ -7,16 +7,6 @@ import numpy as np
 
 class AIFilter:
     def apply_filter(self, image: Image, filter_name: FilterNamesEnum) -> Image:
-        if filter_name == FilterNamesEnum.AI_CANDY:
-            style = "candy"
-        elif filter_name == FilterNamesEnum.AI_FEATHERS:
-            style = "feathers"
-        elif filter_name == FilterNamesEnum.AI_MOSAIC:
-            style = "mosaic"
-        elif filter_name == FilterNamesEnum.AI_STARRY_NIGHT:
-            style = "starry_night"
-        elif filter_name == FilterNamesEnum.AI_WAVE:
-            style = "wave"
         image = np.asarray(image)
-        result = style_gan.run(image, input_size=image.shape[0], style=style)
+        result = style_gan.run(image, input_size=image.shape[0], style=filter_name.value.lower())
         return Image.fromarray(np.uint8(result)).convert('RGB')
