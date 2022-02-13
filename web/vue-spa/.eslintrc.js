948465a41f28d48542bd477c05243ebc2079df63
Anastasia Shreder <dragonsunfire@mail.ru>
2022-02-13 18:44:27 +0300

    Installed jest and cypress

--- web/vue-spa/.eslintrc.js
+++ web/vue-spa/.eslintrc.js
@@ -1,18 +1,36 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
+    'jest/globals': true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  },
 
+  plugins: ['jest'],
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
