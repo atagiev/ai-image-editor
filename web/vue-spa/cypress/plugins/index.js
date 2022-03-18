eee2572b9c0ce6669c17654438cb10241b5b3fc1
Anastasia Shreder <dragonsunfire@mail.ru>
2022-03-18 21:54:18 +0300

    Added 10 tests

---
+++ web/vue-spa/cypress/plugins/index.js
@@ -0,0 +1,12 @@
+const { startDevServer } = require('@cypress/webpack-dev-server')
+// const webpackConfig = require('@vue/cli-service/webpack.config.js')
+
+module.exports = (on, config) => {
+  on('dev-server:start', options =>
+    startDevServer({
+      options
+    })
+  )
+
+  return config
+}
