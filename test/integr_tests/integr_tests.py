19e01079c65cfb29f4831a2833b3f13ac0b52ac7
atagiev <andrej-tagiev@yandex.ru>
2022-03-20 00:12:44 +0300

    13,14 тесты

--- test/integr_tests/integr_tests.py
+++ test/integr_tests/integr_tests.py
@@ -5,6 +5,7 @@ import multiprocessing
 from time import sleep
 
 import requests
+from PIL import Image
 
 
 def backend_process():
@@ -147,6 +148,20 @@ class IntegrationTests(unittest.TestCase):
         self.assertEqual(r['error'], "NO")
         self.assertEqual(r['id'], "2")
 
+        data = {'filter_name': "Candy"}
+        path = os.path.join(pathlib.Path().resolve(), "..", "test_images", "cat_300_100.jpg")
+        payload = {'image': open(path, 'rb')}
+        requests.post(url="http://localhost:5000/", data=data, files=payload)
+        data = {'saved_image_id': "4"}
+        requests.post(url="http://localhost:5000/save_image", data=data)
+
+        response = requests.get(url="http://localhost:5000/get_last_saved")
+
+        r = response.json()
+
+        self.assertEqual(r['error'], "NO")
+        self.assertEqual(r['id'], "4")
+
         th.terminate()
 
     def test_bad_get_last_saved(self):
@@ -175,7 +190,6 @@ class IntegrationTests(unittest.TestCase):
 
         th.terminate()
 
-    @unittest.skip
     def test_reset_applied_filters(self):
         th = self._run_backend()
         path = os.path.join(pathlib.Path().resolve(), "..", "test_images", "fox_320_480.jpg")
@@ -187,10 +201,49 @@ class IntegrationTests(unittest.TestCase):
         data = {'saved_image_id': "2"}
         requests.post(url="http://localhost:5000/save_image", data=data)
 
-    @unittest.skip
-    def test_images_on_file_server(self):
-        pass
+        response = requests.get(url="http://localhost:5000/get_last_saved")
+        r = response.json()
+        self.assertEqual(r['error'], "NO")
+        self.assertEqual(r['id'], "2")
 
+
+        payload = {'image': open(path, 'rb')}
+        data = {'filter_name': "Candy"}
+        requests.post(url="http://localhost:5000/", data=data, files=payload)
+
+        response = requests.get(url="http://localhost:5000/get_last_saved")
+        r = response.json()
+        self.assertEqual(r['error'], "NO")
+        self.assertEqual(r['id'], "2")
+
+        th.terminate()
+
+    def test_images_on_file_server(self):
+        th = self._run_backend()
+
+        requests.get(url="http://localhost:5000/reset")
+
+        r = requests.get(url="http://localhost:8000/")
+        self.assertNotIn("2.jpg", r.text)
+
+        path = os.path.join(pathlib.Path().resolve(), "..", "test_images", "fox_320_480.jpg")
+
+        payload = {'image': open(path, 'rb')}
+        data = {'filter_name': "Candy"}
+        requests.post(url="http://localhost:5000/", data=data, files=payload)
+
+        r = requests.get(url="http://localhost:8000/")
+        self.assertIn("2.jpg", r.text)
+
+        r = requests.get(url="http://localhost:8000/2.jpg")
+        from io import BytesIO
+
+        img = Image.open(BytesIO(r.content))
+        self.assertEqual(img.size[0], 320)
+        self.assertEqual(img.size[1], 480)
+
+        th.terminate()
 
 if __name__ == '__main__':
     unittest.main()
+
